eeca2e4d3b68594259130eade4e888e23170d835
Jarrod Bataille <jtbataille@gmail.com>
2020-09-21 17:57:12 -0400

    creation of controller

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = express.Router();
+
+const burger = require("../models/burger.js");
+
+router.get("/", (req, res) => {
+    burger.selectAll((data) => {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/burgers", (req, res) => {
+    burger.insertOne(["burger_name"], [req.body.burger_name], (result) => {
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/burger/:id", (req, res) => {
+    const condition = "id= " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.updateOne(
+        {
+           devoured: req.body.devoured 
+        },
+        condition,
+        (result) => {
+            if (result.changedRows === 0) {
+                return res.status(404).end();
+            }
+            res.status(200).end();
+        }
+    );
+});
+
+module.exports = router;
